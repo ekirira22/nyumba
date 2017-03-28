477e1ee3833f6ddd21da47d4013c62624f70c4a3
Eric Maranga <ekirira22@gmail.com>
2017-03-28 11:12:07 +0300

    Deleting a property is now possible delete rental button works

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -8,6 +8,13 @@ export default Ember.Component.extend({
     },
     hideImage : function(){
       this.set('isImageShowing',false)
+    },
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this property?')){
+        this.sendAction('destroyRental', rental);
+
+      }
     }
   }
+
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-   return this.store.findAll('rental');
- },
+    return this.store.findAll('rental');
+  },
+
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
